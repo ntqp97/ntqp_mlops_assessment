fb0bdae3cdf9b3c922a7cc4f1cc80af930289590
Nguyen The Quang Phuoc <ntqp97@gmail.com>
2023-03-26 15:28:33 +0700

    refactor code test model onnx

--- test_onnx.py
+++ test_onnx.py
@@ -6,23 +6,15 @@ def test_onnx_model():
     input_size = (224, 224)
     model = ONNXModel()
     preprocessor = ImagePreprocessor(input_size)
-    
-    image_path1 = './images_test/n01667114_mud_turtle.JPEG'
-    expected_class_id = 35
-    input_data = preprocessor.preprocess_numpy(image_path1)
-    prediction = model.predict(input_data.numpy())
-    class_probs = np.exp(prediction)
-    predicted_class_id = np.argmax(class_probs)
-    assert predicted_class_id == expected_class_id, \
-        f"Failed test on image {image_path1}. Expected class id: {expected_class_id}, Predicted class id: {predicted_class_id}"
-    print(f"Passed test on image {image_path1}. Predicted class id: {predicted_class_id}")
 
-    image_path2 = './images_test/n01440764_tench.jpeg'
-    expected_class_id = 0
-    input_data = preprocessor.preprocess_numpy(image_path2)
-    prediction = model.predict(input_data.numpy())
-    class_probs = np.exp(prediction)
-    predicted_class_id = np.argmax(class_probs)
-    assert predicted_class_id == expected_class_id, \
-        f"Failed test on image {image_path2}. Expected class id: {expected_class_id}, Predicted class id: {predicted_class_id}"
-    print(f"Passed test on image {image_path2}. Predicted class id: {predicted_class_id}")
+    image_paths = ['./images_test/n01440764_tench.jpeg', './images_test/n01667114_mud_turtle.JPEG']
+    expected_class_ids = [0, 35]
+
+    for i, image_path in enumerate(image_paths):
+        input_data = preprocessor.preprocess_numpy(image_path)
+        prediction = model.predict(input_data.numpy())
+        class_probs = np.exp(prediction)
+        predicted_class_id = np.argmax(class_probs)
+        assert predicted_class_id == expected_class_ids[i], \
+            f"Failed test on image {image_path}. Expected class id: {expected_class_ids[i]}, Predicted class id: {predicted_class_id}"
+        print(f"Passed test on image {image_path}. Predicted class id: {predicted_class_id}")
